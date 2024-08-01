c1f861e3585941199b9e9e178115fe13bc90a4dd
hlebnoe_pole <glebpavlyuk@gmail.com>
2024-08-01 13:29:14 +0300

    initial commit

---
+++ core/src/ru/samsung/gamestudio/GameSettings.java
@@ -0,0 +1,18 @@
+package ru.samsung.gamestudio;
+
+public class GameSettings {
+
+    public static final int SCREEN_WIDTH = 720;
+    public static final int SCREEN_HEIGHT = 1280;
+
+    public static final String SKIN_PATH = "skin/uiskin.json";
+
+    // Physics settings
+
+    public static final float STEP_TIME = 1f / 60f;
+    public static final int VELOCITY_ITERATIONS = 6;
+    public static final int POSITION_ITERATIONS = 6;
+    public static final float SCALE = 0.05f;
+
+
+}
---
+++ core/src/ru/samsung/gamestudio/MyGdxGame.java
@@ -0,0 +1,55 @@
+package ru.samsung.gamestudio;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import static ru.samsung.gamestudio.GameSettings.*;
+
+public class MyGdxGame extends Game {
+
+	public Skin skin;
+	public OrthographicCamera camera;
+	public FitViewport viewport;
+
+	public World world;
+	private float accumulator;
+	
+	@Override
+	public void create () {
+
+		Box2D.init();
+		world = new World(new Vector2(0, 0), true);
+
+		camera = new OrthographicCamera(SCREEN_WIDTH, SCREEN_HEIGHT);
+		viewport = new FitViewport(SCREEN_WIDTH, SCREEN_HEIGHT, camera);
+
+		skin = new Skin(Gdx.files.internal(SKIN_PATH));
+		camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	}
+
+	@Override
+	public void dispose () {
+		skin.dispose();
+	}
+
+	public void stepWorld() {
+		float delta = Gdx.graphics.getDeltaTime();
+		accumulator += Math.min(delta, 0.25f);
+
+		if (accumulator >= STEP_TIME) {
+			accumulator -= STEP_TIME;
+			world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		}
+	}
+}
